cb64dabcb919e2cefded3c8284ca44549e13e0db
Krzysztof Witczak <kwitczak19@gmail.com>
2017-03-25 14:05:05 +0100

    Turrets follow nearest enemy

---
+++ Assets/Turret.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    private Transform target;
+
+    public float range = 15f;
+    public float turnSpeed = 10f;
+    public string enemyTag = "Enemy";
+    public Transform partToRotate;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+
+    // Find closest enemy and target it
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        GameObject nearestEnemy = null;
+        float shortestDistance = Mathf.Infinity;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        } else
+        {
+            target = null;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (target == null)
+            return;
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+	}
+
+    // Draw turret range on selection
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
