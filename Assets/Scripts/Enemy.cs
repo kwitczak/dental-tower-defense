466100220604f967e84618434e8a6a6957597c12
Krzysztof Witczak <kwitczak19@gmail.com>
2017-04-21 19:46:28 +0200

    Made enemies jumping

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,21 +6,30 @@ public class Enemy : MonoBehaviour {
 
     private Transform target;
     private int wavepointIndex = 0;
+    private Rigidbody rb;
 
     void Start ()
     {
         target = Waypoints.points[0];
+        rb = GetComponent<Rigidbody>();
+        InvokeRepeating("Jump", 1f, 1f);
     }
 
     void Update ()
     {
-        Vector3 dir = target.position - transform.position;
+        Vector3 dir = new Vector3(
+            target.position.x - transform.position.x,
+            0f,
+            target.position.z - transform.position.z
+            );
+        //Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
-        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        if (Vector3.Distance(transform.position, target.position) <= 2f)
         {
             GetNextWaypoint();
         }
+
     } 
 
     void GetNextWaypoint()
@@ -33,5 +42,10 @@ public class Enemy : MonoBehaviour {
 
         wavepointIndex++;
         target = Waypoints.points[wavepointIndex];
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector3(0, 8, 0), ForceMode.Impulse);
     }
 }
