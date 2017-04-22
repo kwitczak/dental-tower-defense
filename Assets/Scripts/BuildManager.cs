e6b71df2cf096324ab00f6a1b4529eca75954417
Krzysztof Witczak <kwitczak19@gmail.com>
2017-04-22 21:21:21 +0200

    Added build effect

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -13,9 +13,6 @@ public class BuildManager : MonoBehaviour {
         instance = this;
     }
 
-    public GameObject standardTurretPrefab;
-    public GameObject missileLauncherPrefab;
-
     public bool CanBuild { get { return turretToBuild != null; } }
     public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } }
 
@@ -25,6 +22,7 @@ public class BuildManager : MonoBehaviour {
         turretToBuild = turret;
     }
 
+    public GameObject buildEffect;
     public void BuildTurretOn (Node node)
     {
         if (!HasMoney)
@@ -38,6 +36,9 @@ public class BuildManager : MonoBehaviour {
         GameObject turret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
         node.turret = turret;
 
+        GameObject effect = (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
+        
         Debug.Log("Turret build! Money left: " + PlayerStats.Money);
     }
 }
