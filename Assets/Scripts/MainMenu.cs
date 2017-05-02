44e60c8a10eb9761f3790d7bd61246d07fc547a0
Krzysztof Witczak <kwitczak19@gmail.com>
2017-05-02 16:40:40 +0200

    Added menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string levelToLoad = "MainLevel";
+
+    public void Play ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    } 
+
+    public void Quit ()
+    {
+        Debug.Log("Exciting...");
+        Application.Quit();
+    } 
+}
