198fbee0f345b5024948c586028de5f6d3e3c18c
Krzysztof Witczak <kwitczak19@gmail.com>
2017-05-20 09:32:51 +0200

    Changes to level 1 and brush

---
+++ Assets/TeethAnimationsPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeethAnimationsPlayer : MonoBehaviour {
+
+    public GameObject Teeths;
+
+	public void Open()
+    {
+        Animator anim = Teeths.GetComponent<Animator>();
+        anim.Play("TeethOpenClose");
+    }
+	
+}
