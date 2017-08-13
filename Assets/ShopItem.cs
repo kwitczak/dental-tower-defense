406d612eb75287e21d3e215560df262c7ddd0c63
Krzysztof Witczak <kwitczak19@gmail.com>
2017-08-13 21:45:36 +0200

    Added hover tips for towers

---
+++ Assets/ShopItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItem : MonoBehaviour {
+
+    public GameObject hoverInfo;
+
+    public void ShowHoverInfo()
+    {
+        hoverInfo.SetActive(true);
+    }
+
+    public void HideHoverInfo()
+    {
+        hoverInfo.SetActive(false);
+    }
+}
